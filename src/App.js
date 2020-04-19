e603613630e11186829182d3b5499ab05a3f1c20
Yang Liu <yang.7.liu@nokia-bell-labs.com>
2020-04-19 15:23:52 +0200

    message explicatif

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur le Blindtest</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai Blindtest !</p>
+        <p>First attemps of modification here.</p>
       </div>
       <div className="App-buttons">
       </div>
